dcfec47952b9dac79db83cc8717b815fdb3b9a81
Aniket511 <55677241+Aniket511@users.noreply.github.com>
2021-11-16 12:00:21 +0530

    Update slotifyCreateWholeDatabaseSoFar.sql

--- MusicPlayer/slotifyCreateWholeDatabaseSoFar.sql
+++ MusicPlayer/slotifyCreateWholeDatabaseSoFar.sql
@@ -1,20 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 27, 2017 at 10:12 AM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `slotify`
