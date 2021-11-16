495ed2b3e24bcd8e46a42d9a95d01f7979f6cc55
Aniket511 <55677241+Aniket511@users.noreply.github.com>
2021-11-16 11:55:17 +0530

    Update SQL complete Data.sql

--- SQL complete Data.sql
+++ SQL complete Data.sql
@@ -1,22 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 14, 2018 at 10:49 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
---
 -- Database: `slotify`
 --
 
